dc35a302ef757a6ed55e4c26301d8d7b729c5382
Bilal <katbi89@gmail.com>
2019-12-12 11:22:33 +0200

    make labels

--- resources/views/receipts/index.blade.php
+++ resources/views/receipts/index.blade.php
@@ -23,24 +23,34 @@
         <div class="col-sm-4 text-right"><h3>وثيقة شحن</h3></div>
       </div>
       <div class="row">
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="المرسل إليه"></div>
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="المرسل"></div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل اسم المرسل إليه"></div>
+        <div class="col-sm-3">المرسل إليه</div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل اسم المرسل"></div>
+        <div class="col-sm-3">المرسل</div>
       </div>
       <div class="row">
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="العنوان"></div>
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="العنوان"></div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل عنوان المرسل إليه"></div>
+        <div class="col-sm-3">العنوان</div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل عنوان المرسل"></div>
+        <div class="col-sm-3">العنوان</div>
       </div>
       <div class="row">
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="الهاتف"></div>
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="الهاتف"></div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل رقم هاتف المرسل إليه"></div>
+        <div class="col-sm-3">الهاتف</div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل رقم هاتف المرسل"></div>
+        <div class="col-sm-3">الهاتف</div>
       </div>
       <div class="row">
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="مكان التسليم"></div>
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="مكان الإستلام"></div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل مكان التسليم"></div>
+        <div class="col-sm-3">مكان التسليم</div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل مكان الإستلام"></div>
+        <div class="col-sm-3">مكان الإستلام</div>
       </div>
       <div class="row">
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="المحتويات"></div>
-        <div class="col-sm-6"><input class="form-control text-center" type="text" placeholder="عدد الطرود"></div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل المحتويات"></div>
+        <div class="col-sm-3">المحتويات</div>
+        <div class="col-sm-3"><input class="form-control text-right" type="text" placeholder="أدخل عدد الطرود"></div>
+        <div class="col-sm-3">عدد الطرود</div>
       </div>
       <div class="row text-center">
             <div class="col-sm-4 text-center"><h3>للتحصيل من المرسل إليه</h3></div>
@@ -49,23 +59,24 @@
       </div>
       <br>
       <div class="row text-center">
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
             <div class="col-sm-4"><h4>أجور الشحن</h4></div>
       </div>
       <div class="row text-center">
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
             <div class="col-sm-4"><h4>متفرقات</h4></div>
       </div>
       <div class="row text-center">
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
-            <div class="col-sm-4"><input class="form-control" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
+            <div class="col-sm-4"><input class="form-control text-right" type="text" placeholder=""></div>
             <div class="col-sm-4"><h4>ضد الشحن</h4></div>
           </div>
           <div class="card-footer text-center">
                 <button type="submit" class="btn btn-info">إرسال</button>
                 <button type="submit" class="btn btn-default float-right">إلغاء</button>
               </div>
+            </div>
 @endsection
 
